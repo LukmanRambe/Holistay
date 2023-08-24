12b26ed27a4251ae6859789c51640f1a29f64047
LukmanRambe <lukmanrambe1945@gmail.com>
2023-08-24 21:27:19 +0700

    style: fix hero responsive

--- src/components/LandingPage/Hero/index.jsx
+++ src/components/LandingPage/Hero/index.jsx
@@ -4,7 +4,7 @@ import IconTreasures from "assets/icons/icon-treasures.svg";
 import ImageHeroFrame from "assets/images/img-hero-frame.jpg";
 import ImageHero from "assets/images/img-hero.jpg";
 import Button from "components/Artifacts/Button";
-import { Fade, Slide } from "react-reveal";
+import { Fade } from "react-reveal";
 import formatNumber from "utils/formatNumber";
 
 const Hero = ({ refMostPicked, data }) => {
@@ -18,20 +18,20 @@ const Hero = ({ refMostPicked, data }) => {
 	return (
 		<section className="w-full px-6 xl:px-36 pt-[50px] md:pt-[70px] lg:px-10">
 			<section className="flex items-start justify-between">
-				<Slide left>
-					<article className="w-[500px] flex flex-col justify-between">
-						<article className="mb-[70px] lg:mb-24">
+				<Fade bottom>
+					<article className="sm:w-[500px] w-full flex flex-col sm:justify-between">
+						<article className="mb-[70px] lg:mb-24 text-center sm:text-start">
 							<h1 className="mb-5 text-[38px] xl:text-[42px] leading-[63px] font-bold -text--primary-dark">
 								Forget Busy Work, <br /> Start Next Vacation
 							</h1>
 
-							<p className="w-3/4 mb-8 font-light -text--gray">
+							<p className="w-full mb-8 font-light sm:w-3/4 -text--gray">
 								We provide what you need to enjoy your vacation with family.
 								Time to make another memorable moments.
 							</p>
 
 							<Button
-								className="min-h-[50px] min-w-[240px]"
+								className="min-h-[50px] w-full sm:max-w-[240px] sm:min-w-[240px]"
 								hasShadow
 								isPrimary
 								onClick={showMostPicked}>
@@ -39,7 +39,7 @@ const Hero = ({ refMostPicked, data }) => {
 							</Button>
 						</article>
 
-						<article className="w-full flex flex-col md:flex-row gap-8 md:gap-[50px]">
+						<article className="w-full flex gap-6 sm:gap-8 xl:gap-[50px]">
 							<div className="w-full">
 								<img
 									width="36"
@@ -48,7 +48,7 @@ const Hero = ({ refMostPicked, data }) => {
 									src={IconTravelers}
 									alt={`${formatNumber(data.travelers)} Travelers`}
 								/>
-								<h3 className="font-medium mt-[10px]">
+								<h3 className="font-medium mt-[10px] flex flex-col sm:flex-row gap-0 sm:gap-1">
 									{formatNumber(data.travelers)}{" "}
 									<span className="font-light -text--gray">Travelers</span>
 								</h3>
@@ -61,7 +61,7 @@ const Hero = ({ refMostPicked, data }) => {
 									src={IconTreasures}
 									alt={`${formatNumber(data.treasures)} Treasures`}
 								/>
-								<h3 className="font-medium mt-[10px]">
+								<h3 className="font-medium mt-[10px] flex flex-col sm:flex-row gap-0 sm:gap-1">
 									{formatNumber(data.treasures)}{" "}
 									<span className="font-light -text--gray">Treasures</span>
 								</h3>
@@ -74,28 +74,28 @@ const Hero = ({ refMostPicked, data }) => {
 									src={IconCities}
 									alt={`${formatNumber(data.cities)} Cities`}
 								/>
-								<h3 className="font-medium mt-[10px]">
+								<h3 className="font-medium mt-[10px] flex flex-col sm:flex-row gap-0 sm:gap-1">
 									{formatNumber(data.cities)}{" "}
 									<span className="font-light -text--gray">Cities</span>
 								</h3>
 							</div>
 						</article>
 					</article>
-				</Slide>
+				</Fade>
 
 				<Fade bottom>
-					<article className="hidden lg:block w-[400px] lg:w-[430px] xl:w-[560px] h-fit">
-						<div className="relative pt-[40px]">
+					<article className="hidden lg:block min-w-[400px] lg:min-w-[470px] 2xl:min-w-[540px] h-fit">
+						<div className="relative pt-[60px] 2xl:pt-[40px]">
 							<img
 								src={ImageHero}
 								alt="Home with balcony"
-								className="absolute m-0 -mt-[40px] -ml-[40px] z-[1]"
+								className="absolute -mt-[30px] lg:mr-[30px] z-[1] right-0"
 							/>
 
 							<img
 								src={ImageHeroFrame}
 								alt="Home with balcony frame"
-								className="absolute m-0 -mr-[15px] -mb-[15px]"
+								className="absolute mt-[5px] right-0"
 							/>
 						</div>
 					</article>
